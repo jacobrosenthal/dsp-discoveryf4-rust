1380019afed21d1e1459c7ccf63deacc44471ff4
Jacob Rosenthal <jacobrosenthal@gmail.com>
2020-06-13 11:34:57 -0700

    remove inout functions in heapless

--- lab2/examples/2_11_heapless_frame_based_systems.rs
+++ lab2/examples/2_11_heapless_frame_based_systems.rs
@@ -18,6 +18,8 @@ use panic_rtt as _;
 mod utils;
 
 use heapless::consts::U10;
+use itertools::Itertools;
+
 const N: usize = 10;
 
 macro_rules! dbgprint {
@@ -45,83 +47,176 @@ fn main() -> ! {
         .freeze();
 
     //y[n] = b x[n]
-    let digital_system1 = utils::unit_step(0..N)
-        .map(|unit| 2.2 * unit)
+    let y1 = utils::unit_step(0..N)
+        .map(|u| 2.2 * u)
         .collect::<heapless::Vec<f32, U10>>();
-    dbgprint!("digital_system1: {:?}", &digital_system1[..]);
+    dbgprint!("digital_system1: {:?}", &y1);
 
     //y[n] = x1[n] + x2[n]
-    let digital_system2 = utils::unit_step(0..N)
-        .zip(utils::sinusoidal(0..N))
-        .map(|(unit, s)| unit + s)
+    let y2 = utils::sinusoidal(0..N)
+        .zip(utils::unit_step(0..N))
+        .map(|(inny1, inny2)| inny1 + inny2)
         .collect::<heapless::Vec<f32, U10>>();
-    dbgprint!("digital_system2: {:?}", &digital_system2[..]);
+    dbgprint!("digital_system2: {:?}", &y2);
 
     //y[n] = x^2[n]
-    let digital_system3 = utils::sinusoidal(0..N)
-        .map(|s| s * s)
+    let y3 = utils::sinusoidal(0..N)
+        .map(|inny| inny * inny)
         .collect::<heapless::Vec<f32, U10>>();
-    dbgprint!("digital_system3: {:?}", &digital_system3[..]);
+    dbgprint!("digital_system3: {:?}", &y3);
 
     //y[n] = b0 x[n] + b1 x[n-1]
-    //random backwards access.. so no iterators unless ... todo
-    let mut digital_system4 = [0f32; N];
-    let sinusoidal = utils::sinusoidal(0..N).collect::<heapless::Vec<f32, U10>>();
-    digital_system4
-        .iter_mut()
-        .enumerate()
-        .for_each(|(idx, out)| {
-            if idx == 0 {
-                *out = 2.2 * sinusoidal[idx]
-            } else {
-                *out = 2.2 * sinusoidal[idx] + -1.1 * sinusoidal[idx - 1]
-            }
-        });
-    dbgprint!("digital_system4: {:?}", &digital_system4[..]);
+    let y4 = DigitalSystem4::new(utils::sinusoidal(0..N)).collect::<heapless::Vec<f32, U10>>();
+    dbgprint!("digital_system4: {:?}", &y4);
 
     //y[n] = b0 x[n] + b1 x[n-1] + a1 y[n-1]
-    //random backwards access.. so no iterators unless ... todo
-    let mut digital_system5 = [0f32; N];
-    let sinusoidal = utils::sinusoidal(0..N).collect::<heapless::Vec<f32, U10>>();
-    //cant enumerate over output either since random access
-    for idx in 0..digital_system5.len() {
-        if idx == 0 {
-            digital_system5[idx] = 2.2 * sinusoidal[idx];
-        } else {
-            digital_system5[idx] =
-                2.2 * sinusoidal[idx] + -1.1 * sinusoidal[idx - 1] + 0.7 * digital_system5[idx - 1];
-        }
-    }
-    dbgprint!("digital_system5: {:?}", &digital_system5[..]);
+    let y5 = DigitalSystem5::new(utils::sinusoidal(0..N)).collect::<heapless::Vec<f32, U10>>();
+    dbgprint!("digital_system5: {:?}", &y5);
 
     //y[n] = b0 x[n+1] + b1 x[n]
-    //digital_system6 in c version has oob array access, should be if (n+1 < size) so y6[9] undefined
-    let unit_pulse = utils::unit_step(0..N).collect::<heapless::Vec<f32, U10>>();
-    let digital_system6 = unit_pulse
-        .windows(2)
-        .map(|unit_window| 2.2 * unit_window[1] + -1.1 * unit_window[0])
+    // digital_system6 in c version has oob array access, should be if (n+1 < size) so y6[9] undefined
+    let y6 = utils::unit_step(0..N)
+        .tuple_windows()
+        .map(|(u0, u1)| 2.2 * u1 + -1.1 * u0)
         .collect::<heapless::Vec<f32, U10>>();
-    dbgprint!("digital_system6: {:?}", &digital_system6[..]);
+    dbgprint!("digital_system6: {:?}", &y6);
 
     //y[n] = b0 x[n] + a1 y[n-1]
-    //random backwards access.. so no iterators unless ... todo
-    let mut digital_system7 = [0f32; N];
-    let unit_pulse = utils::unit_pulse(0..N).collect::<heapless::Vec<f32, U10>>();
-    for idx in 0..digital_system5.len() {
-        if idx == 0 {
-            digital_system7[idx] = 1.0 * unit_pulse[idx]
-        } else {
-            digital_system7[idx] = 1.0 * unit_pulse[idx] + 2.0 * digital_system7[idx - 1]
-        }
-    }
-    dbgprint!("digital_system7: {:?}", &digital_system7[..]);
+    let y7 = DigitalSystem7::new(utils::unit_pulse(0..N)).collect::<heapless::Vec<f32, U10>>();
+    dbgprint!("digital_system7: {:?}", &y7);
 
     //y[n] = n x[n]
-    let digital_system8 = utils::sinusoidal(0..N)
+    let y8 = utils::sinusoidal(0..N)
         .enumerate()
-        .map(|(idx, s)| idx as f32 * s)
+        .map(|(idx, inny)| idx as f32 * inny)
         .collect::<heapless::Vec<f32, U10>>();
-    dbgprint!("digital_system8: {:?}", &digital_system8[..]);
+
+    dbgprint!("digital_system8: {:?}", &y8);
 
     loop {}
+}
+
+struct DigitalSystem4<I>
+where
+    I: Iterator<Item = f32>,
+{
+    last: Option<f32>,
+    iter: I,
+}
+impl<I> DigitalSystem4<I>
+where
+    I: Iterator<Item = f32>,
+{
+    fn new(iter: I) -> Self {
+        Self {
+            last: None,
+            iter: iter,
+        }
+    }
+}
+
+impl<I> Iterator for DigitalSystem4<I>
+where
+    I: Iterator<Item = f32>,
+{
+    type Item = f32;
+
+    fn next(&mut self) -> Option<f32> {
+        if let Some(val) = self.iter.next() {
+            let abc = if let Some(last) = self.last {
+                2.2 * val + -1.1 * last
+            } else {
+                2.2 * val
+            };
+
+            self.last = Some(val);
+            Some(abc)
+        } else {
+            None
+        }
+    }
+}
+
+struct DigitalSystem5<I>
+where
+    I: Iterator<Item = f32>,
+{
+    last_in: Option<f32>,
+    last_out: Option<f32>,
+    iter: I,
+}
+
+impl<I> DigitalSystem5<I>
+where
+    I: Iterator<Item = f32>,
+{
+    fn new(iter: I) -> Self {
+        Self {
+            last_in: None,
+            last_out: None,
+            iter: iter,
+        }
+    }
+}
+
+impl<I> Iterator for DigitalSystem5<I>
+where
+    I: Iterator<Item = f32>,
+{
+    type Item = f32;
+
+    fn next(&mut self) -> Option<f32> {
+        if let Some(val) = self.iter.next() {
+            let abc = if let (Some(last_in), Some(last_out)) = (self.last_in, self.last_out) {
+                2.2 * val + -1.1 * last_in + -1.1 * last_out
+            } else {
+                2.2 * val
+            };
+
+            self.last_in = Some(val);
+            Some(abc)
+        } else {
+            None
+        }
+    }
+}
+
+struct DigitalSystem7<I>
+where
+    I: Iterator<Item = f32>,
+{
+    last_out: Option<f32>,
+    iter: I,
+}
+
+impl<I> DigitalSystem7<I>
+where
+    I: Iterator<Item = f32>,
+{
+    fn new(iter: I) -> Self {
+        Self {
+            last_out: None,
+            iter: iter,
+        }
+    }
+}
+impl<I> Iterator for DigitalSystem7<I>
+where
+    I: Iterator<Item = f32>,
+{
+    type Item = f32;
+
+    fn next(&mut self) -> Option<f32> {
+        if let Some(val) = self.iter.next() {
+            self.last_out = if let Some(last_out) = self.last_out {
+                Some(1.0 * val + 2.0 * last_out)
+            } else {
+                Some(1.0 * val)
+            };
+
+            self.last_out
+        } else {
+            None
+        }
+    }
 }
--- lab2/examples/utils/mod.rs
+++ lab2/examples/utils/mod.rs
@@ -3,22 +3,27 @@ use micromath::F32Ext;
 const A: f32 = 0.8;
 const W0: f32 = core::f32::consts::PI / 5f32;
 
+#[allow(unused)]
 pub fn unit_pulse(range: core::ops::Range<usize>) -> impl Iterator<Item = f32> {
     range.map(|val| if val == 0 { 1.0 } else { 0.0 })
 }
 
+#[allow(unused)]
 pub fn unit_step(range: core::ops::Range<usize>) -> impl Iterator<Item = f32> {
     range.map(|_| 1.0)
 }
 
+#[allow(unused)]
 pub fn unit_ramp(range: core::ops::Range<usize>) -> impl Iterator<Item = f32> {
     range.map(|val| val as f32)
 }
 
+#[allow(unused)]
 pub fn sinusoidal(range: core::ops::Range<usize>) -> impl Iterator<Item = f32> {
     range.map(|val| (W0 * val as f32).sin())
 }
 
+#[allow(unused)]
 pub fn exponential(range: core::ops::Range<usize>) -> impl Iterator<Item = f32> {
     range.map(|val| A.powf(val as f32))
 }
